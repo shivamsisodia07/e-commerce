e9a8eccea726d283a2ce01f4452fba2c924fd27c
shivam <shivamsisodia8656816@gmail.com>
2021-10-28 21:15:47 +0530

    added 4 new html pages and modified css and js  renamed myntra to Home

--- myntra.js
+++ myntra.js
@@ -1,4 +1,6 @@
-                                                       
+    
+   
+
 $('.single').slick({
   slidesToShow: 1,                                         //banner slider
   slidesToScroll: 1,
@@ -24,7 +26,7 @@ $('.slick').slick({
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
+          slidesToShow: 5,
           slidesToScroll: 3,
           infinite: true,
           dots: true
@@ -33,14 +35,14 @@ $('.slick').slick({
       {
         breakpoint: 600,
         settings: {
-          slidesToShow: 2,
+          slidesToShow: 4,
           slidesToScroll: 2
         }
       },
       {
         breakpoint: 480,
         settings: {
-          slidesToShow: 1,
+          slidesToShow: 3,
           slidesToScroll: 1
         }
       }
